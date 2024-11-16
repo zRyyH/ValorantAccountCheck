1a4aa4791be14608e8495a7f4d2065115edaafd7
Ryan Carlos Gadelha <zryyh.br@gmail.com>
2024-11-16 04:11:58 -0300

    UPDATE V1

---
+++ main.py
@@ -0,0 +1,64 @@
+import requests
+import traceback
+from valorant_api import ultima_atividade, ultimo_rank
+
+
+# URL para a requisição
+url = "https://api.awpsoft.com.br/api/"
+
+
+def getAccounts():
+    try:
+        # Fazer a requisição GET
+        response = requests.get(url+'accounts')
+
+        # Verificar o status da resposta
+        if response.status_code == 200:
+            accounts = response.json()  # Se o retorno for JSON
+
+            if accounts:
+                return accounts
+        else:
+            print("Erro ao fazer a requisição. Código:", response.status_code)
+    
+    except:
+        traceback.print_exc()
+
+
+def postAccount(**kwargs):
+    try:
+        print(kwargs)
+        payload = {}
+        payload.update(kwargs)
+
+
+        # Fazer a requisição GET
+        response = requests.put(url+'account', json=payload)
+        print(response)
+
+        if response:
+            pass
+        else:
+            print("Erro ao fazer a requisição. Código:", response.status_code)
+
+    except:
+        traceback.print_exc()
+
+
+
+accounts = getAccounts()
+
+for account in accounts:
+    try:
+        nametag = account['nametag']
+
+        account['ultimaAtividade'] = ultima_atividade(nametag)
+        account['elo'] = ultimo_rank(nametag)
+
+        account.pop('dataAtualizacao')
+        account.pop('dataRegistro')
+
+        postAccount(**account)
+    
+    except:
+        traceback.print_exc()
---
+++ valorant_api.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import requests
+import traceback
+
+headers = {
+    "Authorization": 'HDEV-97da4e27-72ac-48c2-8afd-18e2d509b63b'
+}
+
+def ultima_atividade(nametag):
+    try:
+        if nametag:
+            name, tag = nametag.split('#')
+            url = f"https://api.henrikdev.xyz/valorant/v4/matches/na/pc/{name}/{tag}"
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                data_iso = response.json()['data'][0]['metadata']['started_at']
+                data_datetime = datetime.strptime(data_iso, "%Y-%m-%dT%H:%M:%S.%fZ")
+                data_formatada = data_datetime.strftime("%d/%m/%Y")
+                timestamp = datetime.strptime(data_formatada, "%d/%m/%Y").timestamp()
+                return data_formatada
+        return '????', 0
+    except:
+        traceback.print_exc()
+        return '????', 0
+    
+def ultimo_rank(nametag):
+    try:
+        if nametag:
+            name, tag = nametag.split('#')
+            url = f"https://api.henrikdev.xyz/valorant/v1/mmr-history/na/{name}/{tag}"
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                data = response.json()['data'][0]
+                last_rank = data['currenttierpatched']
+                return last_rank
+            return '????'
+    except:
+        traceback.print_exc()
+        return '????'
